264160ad118da2f218a503114032f23e2fe666e4
duttaarun <duttaarun2015@gmail.com>
2022-05-31 21:15:13 +0100

    Prize worth .001 eth per contract deployed

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,6 +6,8 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
 
+    uint256 private seed;
+
     event NewWave(address indexed from, uint256 timestamp, string message);
 
     struct Wave {
@@ -16,15 +18,42 @@ contract WavePortal {
 
     Wave[] waves;
 
-    constructor() {
-        console.log("This is a POC contract");
+    mapping(address => uint256) public lastWavedAt;
+
+    constructor() payable {
+        console.log("This is a Wave POC contract");
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function wave(string memory _message) public {
+
+        require(
+            lastWavedAt[msg.sender] + 1 minutes < block.timestamp,
+            "Wait 1m"
+        );
+
         totalWaves += 1;
         console.log("%s waved w/ message %s", msg.sender, _message);
 
         waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+
+        console.log("Random # generated: %d", seed);
+
+        lastWavedAt[msg.sender] = block.timestamp;
+
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.00022 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
 
         //To emit and save event on Blockchain
         emit NewWave(msg.sender, block.timestamp, _message);
